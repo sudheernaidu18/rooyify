d1c691f4085499d446b6eb52902e0c83c9db9480
reeth <reeth@users.noreply.github.com>
2026-07-30 11:34:05 +0530

    chore: update RetrofitClient BASE_URL to point to working Serveo tunnel URL

--- app/src/main/java/com/example/rooyify/network/RetrofitClient.kt
+++ app/src/main/java/com/example/rooyify/network/RetrofitClient.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
     // Hosted production backend URL (Exposed via live secure tunnel)
-    private const val BASE_URL = "https://04e374d294a84502-180-235-121-242.serveousercontent.com/"
+    private const val BASE_URL = "https://92d58568327b9116-180-235-121-242.serveousercontent.com/"
 
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
